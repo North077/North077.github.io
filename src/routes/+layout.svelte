fd2d8438b1b65cfe0a29b4d2b8de3f32c03baa4f
North <deykiron365@gmail.com>
2023-06-26 16:33:47 +0530

    Added Site content

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import '@fontsource/m-plus-rounded-1c/700.css';
+  import '@fontsource/varela-round';
+</script>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="shape" id="shape-1"></div>
+<div class="shape" id="shape-2"></div>
+
+<div class="banner">
+  <h1>This Website is under <br> Construction. </h1>
+  <p>Stay tuned!</p>
+</div>
+
+<style>
+  
+  :global( body ) {
+    background: #EBEBF6;
+  }
+   
+  
+  .banner {
+    width: 86vw;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate( -50%, -50% );
+  }
+  
+  h1 {
+    color: #444444;
+    font-family: 'M PLUS Rounded 1c', sans-serif;
+    font-size: 24pt;
+  }
+  
+  p {
+    color: #7c7c7c;
+    font-family: 'Varela Round', sans-serif;
+    font-size: 14pt;
+    letter-spacing: 8.5pt;
+    transform: translateY(-14px);
+  }
+  
+  .shape {
+    height: 180px;
+    width: 130px;
+    border-radius: 50%;
+    filter: blur(65px);
+    position: absolute;
+  }
+  
+  #shape-1 {
+    background: #FF4444;
+    top: 5%;
+    right: 5%;
+  }
+  
+  #shape-2 {
+    background: #6DFFF4;
+    bottom: 5%;
+    left: 5%;
+  }
+  
+</style>
